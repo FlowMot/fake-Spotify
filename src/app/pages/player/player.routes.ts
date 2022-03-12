2b931ffa817186c341b186680d74c153dd88a48e
FlowMot <augusto9812@hotmail.com>
2022-03-12 18:06:22 -0300

    Commit inicial

---
+++ src/app/pages/player/player.routes.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { Routes } from "@angular/router";
+import { PlayerComponent } from "./player.component";
+
+export const PlayerRotas: Routes = [
+    {
+        path: '',
+        component: PlayerComponent
+        
+    }
+]
